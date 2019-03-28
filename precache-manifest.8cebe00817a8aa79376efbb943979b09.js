289ee8521c1ad52f3ebda61abf56c2651d407a2d
annabytnar <akbytnar@gmail.com>
2019-03-28 07:34:00 +0100

    Updates

---
+++ precache-manifest.8cebe00817a8aa79376efbb943979b09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "45361b0bd979fdd3606e",
+    "url": "/famly/static/css/main.2ab0caed.chunk.css"
+  },
+  {
+    "revision": "45361b0bd979fdd3606e",
+    "url": "/famly/static/js/main.214d1357.chunk.js"
+  },
+  {
+    "revision": "af6df4bc663e18d37b25",
+    "url": "/famly/static/js/runtime~main.78334cc2.js"
+  },
+  {
+    "revision": "7c495df0ac37171baf71",
+    "url": "/famly/static/css/2.64f91000.chunk.css"
+  },
+  {
+    "revision": "7c495df0ac37171baf71",
+    "url": "/famly/static/js/2.521dfeda.chunk.js"
+  },
+  {
+    "revision": "d77788982f1f72035e5c5a3bf76ba401",
+    "url": "/famly/index.html"
+  }
+];
